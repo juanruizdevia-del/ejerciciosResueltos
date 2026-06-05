903dfd24041b2ba7790e7d4c0d82672fb689cb82
Juan Ruiz <juanruizdevia@gmail.com>
2026-06-05 17:45:25 -0500

    19.2-InteraccionComponentesHTML has been added

---
+++ 19.2-InteraccionComponentesHTML/script.js
@@ -0,0 +1,10 @@
+
+const description = document.getElementById('description').value;
+const category = document.getElementById('category').value;
+
+class task{
+    constructor(description,category){
+        this.description=description;
+        this.category=category;
+    }
+}
