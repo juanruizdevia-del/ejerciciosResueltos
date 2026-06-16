85d26b38fb320df1809f98a1906d04d685c1a33c
Juan Ruiz <juanruizdevia@gmail.com>
2026-06-15 20:50:03 -0500

    23.4-gestion-eventos has been finished

--- 23.4-gestion-eventos/scritp.js
+++ 23.4-gestion-eventos/scritp.js
@@ -1,11 +1,16 @@
 
-const add_product = document.querySelectorAll('.btn-agregar');
+let cantidadItems = 0;
+let totalGeneral = 0.0;
 
-add_product.forEach((btn) => {
+const addProduct = document.querySelectorAll('.btn-agregar');
+
+addProduct.forEach((btn) => {
     btn.addEventListener('click', (evt) => {
         const name = evt.currentTarget.dataset.nombre;
         const price = parseFloat(evt.currentTarget.dataset.precio);
         console.log(`Nombre: ${name} | precio: ${price} han sido seleccionados.`);
+        cantidadItems++;
+        totalGeneral += price;
         addToCard(name, price);
     })
 });
@@ -22,10 +27,42 @@ function addToCard(name, price) {
     const newLi = document.createElement('li');
     const data = document.createTextNode(`Nombre: ${name} | precio: ${price} `);
     const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Eliminar';
-    deleteBtn.className = 'btn btn-primary btn-sm btn-dark m-1'
+    deleteBtn.textContent = 'X';
 
+    deleteBtn.className = 'btn-delete btn-primary btn-sm btn-dark m-1'
+    newLi.append(data, deleteBtn);
+    document.getElementById('lista-carrito').appendChild(newLi);
+    
+    updateBadge();
+    updateTotal();
 
-    newLi.append(data,deleteBtn);
-    document.getElementById('lista-carrito').prepend(newLi);
+    deleteBtn.addEventListener('click', () => {
+        document.getElementById('lista-carrito').removeChild(newLi);
+        console.log(`${name} ha sido eliminado de la lista.`);
+        cantidadItems--;
+        totalGeneral -= price;
+        updateBadge();
+        updateTotal();
+    });
 }
+
+
+function updateBadge() {
+    const badge = document.getElementById('badge');
+    badge.textContent = cantidadItems;
+}
+
+function updateTotal() {
+    const total = document.getElementById('total');
+    total.textContent = totalGeneral;
+}
+
+const deleteCart = document.getElementById('btn-delete-cart');
+
+deleteCart.addEventListener('click',()=>{
+    document.getElementById('lista-carrito').innerHTML = '';
+    cantidadItems = 0;
+    updateBadge();
+    totalGeneral = 0.0;
+    updateTotal();
+});
